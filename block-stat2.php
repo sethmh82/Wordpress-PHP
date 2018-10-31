47e149bf9c768aad4e8fb7e5d50c093f5021cfc7
Seth Harden <sethmh82@users.noreply.github.com>
2018-10-31 11:49:56 -0700

    Create block-stat2.php

---
+++ block-stat2.php
@@ -0,0 +1,6 @@
+<?php if( $allxusr   ||  in_category( 61 )  ||  in_category( 64 )   ||  in_category( 63 )  ||  in_category( 74 )   ||  in_category( 45 )  ||  in_category( 70 )  ||  in_category( 64 )  ||  in_category( 65 )  ||  in_category( 81 )  ||  in_category( 10 )  ): ?>
+<div id="secondstat">
+<?php the_field('stat_sec2') ?>
+</div>
+<?php endif; ?> 
+<?php endif; ?> 
